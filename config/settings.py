5efc9fccee03fc38f5cb12e96b5e21bd9450edeb
shaikhaman284 <shaikhamanksf@gmail.com>
2025-11-12 21:01:13 +0530

    Ready for deployment

--- config/settings.py
+++ config/settings.py
@@ -165,3 +165,13 @@ CORS_ALLOW_CREDENTIALS = True
 
 # Firebase
 FIREBASE_CREDENTIALS_PATH = config('FIREBASE_CREDENTIALS_PATH', default='firebase-credentials.json')
+
+
+# Production settings
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    X_FRAME_OPTIONS = 'DENY'
